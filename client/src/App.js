3740f2a6a3334e5d81a73db0b9b75f05c1ef3607
Famondir <43599267+Famondir@users.noreply.github.com>
2024-03-04 18:03:22 +0100

    Implemented DataTable view of comparisons from csv

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,71 @@
 import React, { useState, useEffect } from "react";
+import DataTable from 'react-data-table-component';
+
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState()
+
+  const [records, setRecords] = useState()
+
+  const columns = [
+    {
+      name: 'Datum',
+      selector: row => row.Datum,
+      sortable: true
+    },
+    {
+      name: 'Gewinner',
+      selector: row => row.Gewinner,
+      sortable: true
+    },
+    {
+      name: 'Verlierer',
+      selector: row => row.Verlierer,
+      sortable: true
+    },
+  ];
+
+  const customStyles = {
+    rows: {
+      style: {
+        minHeight: '72px', // override the row height
+      },
+    },
+    headCells: {
+      style: {
+        fontSize: '1.25rem',
+        fontWeight: 550,
+      },
+    },
+    cells: {
+      style: {
+        fontSize: '1rem',
+      },
+    },
+  };
+
+  function handleFilter(event) {
+    const newData = data.filter(row => {
+      return (
+        row.Gewinner.toLowerCase().includes(event.target.value.toLowerCase()) || 
+        row.Verlierer.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+    })
+    setRecords(newData)
+  }
 
   useEffect(() => {
-    fetch("/api/members").then(
-      res => res.json()
+    fetch("/api/load_csv_as_dataframe/vergleiche.csv").then(
+      res => {
+        if (res.ok) {
+          return res.json()
+        }
+      }
     ).then(
       data => {
         setData(data)
+        setRecords(data)
         console.log(data)
       }
     )
@@ -18,12 +74,22 @@ function App() {
   return (
     <div>
 
-      {(typeof data.members === 'undefined') ? (
+      {(typeof data === 'undefined') ? (
         <p>Loading...</p>
       ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
+        <div>
+          <div>
+            <span>Suche Lied: </span><input type="text" onChange={handleFilter}></input>
+          </div>
+          <DataTable
+            columns={columns}
+            data={records}
+            customStyles={customStyles}
+            theme="light"
+            fixedHeader
+            pagination
+          ></DataTable>
+        </div>
       )}
 
     </div>
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
 
 # Members API Route
-@app.route("/api/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+@app.route("/api/load_csv_as_dataframe/<string:filename>")
+def read_csv_as_dataframe(filename):
+    return pd.read_csv("../trueskill_tt/"+filename).to_json(orient='records')
+    # return {"members": ["Member1", "Member2", "Member3"]}
 
 
 if __name__ == "__main__":
