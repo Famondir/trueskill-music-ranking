6ea5b915e138b6b91a8c9dae8bff2894414c10e8
Famondir <43599267+Famondir@users.noreply.github.com>
2024-03-06 12:20:06 +0100

    Styled fronted with bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React, { useState, useEffect } from "react";
 import DataTable from 'react-data-table-component';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.css';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import CardBody from "react-bootstrap/esm/CardBody";
+import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
+import Image from 'react-bootstrap/Image';
+
 
 function App() {
 
-  const [data, setData] = useState()
+  const [competitionData, setCompetitionData] = useState()
+  const [competitionRecords, setCompetitionRecords] = useState()
 
-  const [records, setRecords] = useState()
+  const [songData, setSongData] = useState()
+  const [songRecords, setSongRecords] = useState()
 
-  const columns = [
+  const [lied, setData] = useState([])
+
+  const competitionColumns = [
     {
       name: 'Datum',
       selector: row => row.Datum,
@@ -26,12 +44,35 @@ function App() {
     },
   ];
 
-  const customStyles = {
-    rows: {
-      style: {
-        minHeight: '72px', // override the row height
-      },
+  const songColumns = [
+    {
+      name: 'Lied',
+      selector: row => row.Liedanfang,
+      sortable: true
     },
+    {
+      name: 'Wertung',
+      selector: row => row.Wertung,
+      sortable: true
+    },
+    {
+      name: 'Unsicherheit',
+      selector: row => row.Unsicherheit,
+      sortable: true
+    },
+    {
+      name: 'Quelle',
+      selector: row => row.Quelle,
+      sortable: true
+    },
+    {
+      name: 'Seitenzahl',
+      selector: row => row.Seite,
+      sortable: true
+    },
+  ];
+
+  const customStyles = {
     headCells: {
       style: {
         fontSize: '1.25rem',
@@ -45,14 +86,100 @@ function App() {
     },
   };
 
-  function handleFilter(event) {
+  const partial = (func, ...args) => (...rest) => func(...args, ...rest);
+
+  function handleFilter(data, setData, row_name_1, row_name_2, event) {
     const newData = data.filter(row => {
       return (
-        row.Gewinner.toLowerCase().includes(event.target.value.toLowerCase()) || 
-        row.Verlierer.toLowerCase().includes(event.target.value.toLowerCase())
+        row[row_name_1].toLowerCase().includes(event.target.value.toLowerCase()) || 
+        row[row_name_2]?.toLowerCase().includes(event.target.value.toLowerCase())
       )
     })
-    setRecords(newData)
+    setData(newData)
+  }
+
+  function SongCard({lied, quelle, seite}) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+      <>
+      <Card>
+        <a href="#" onClick={handleShow}>
+          <Card.Img variant="top" src={"images/songs/"+lied.toLowerCase().replaceAll(",", "").replaceAll(" ", "_")+".png"} />
+        </a>
+        <CardBody>
+          <Card.Title>{lied}</Card.Title>
+          <Card.Text>
+        
+          </Card.Text>
+          <Table striped bordered hover>
+              <tbody>
+                <tr>
+                  <th>Quelle:</th><td>{quelle}</td>
+                </tr>
+                <tr>
+                  <th>Seite:</th><td>{seite}</td>
+                </tr>
+              </tbody>
+            </Table>
+        </CardBody>
+        <Card.Footer className="d-grid">
+          <Button
+            variant="primary"
+            size="lg"
+            onClick={() => post_winner(lied)}
+            >
+            {lied}
+          </Button>
+        </Card.Footer>
+      </Card>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="xl"
+      >
+      <Modal.Header closeButton>
+        <Modal.Title>{lied}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Image 
+          variant="top" 
+          src={"images/songs/"+lied.toLowerCase().replaceAll(",", "").replaceAll(" ", "_")+".png"} 
+          fluid
+          className="modal-height-limit"
+        />
+      </Modal.Body>
+      </Modal>
+      </>
+    )
+  }
+
+  const handleCompetitionFilter = partial(handleFilter, competitionData, setCompetitionRecords, "Gewinner", "Verlierer")
+  const handleSongFilter = partial(handleFilter, songData, setSongRecords, "Liedanfang", "")
+
+  async function post_winner(content) {
+    fetch("api/declare_competition_winner", {
+      method: "POST",
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(content)
+    }).then(
+      res => {
+        if (res.ok) {
+          return res.json()
+        }
+      }
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
   }
 
   useEffect(() => {
@@ -64,35 +191,125 @@ function App() {
       }
     ).then(
       data => {
+        setCompetitionData(data)
+        setCompetitionRecords(data)
+        // console.log(data)
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    fetch("/api/load_song_rating").then(
+      res => {
+        if (res.ok) {
+          return res.json()
+        }
+      }
+    ).then(
+      data => {
+        setSongData(data)
+        setSongRecords(data)
+        // console.log(data)
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    fetch("/api/get_first_competition").then(
+      res => {
+        if (res.ok) {
+          return res.json()
+        }
+      }
+    ).then(
+      data => {
+        console.log(data[0].Liedanfang)
         setData(data)
-        setRecords(data)
-        console.log(data)
       }
     )
   }, [])
 
   return (
-    <div>
+    <Container>
+      <Tabs
+        defaultActiveKey="competition"
+        id=""
+        className="mb-3"
+      >
+        <Tab eventKey="competition" title="Rate next!">
+          <Row id="competition">
 
-      {(typeof data === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          <div>
-            <span>Suche Lied: </span><input type="text" onChange={handleFilter}></input>
-          </div>
-          <DataTable
-            columns={columns}
-            data={records}
-            customStyles={customStyles}
-            theme="light"
-            fixedHeader
-            pagination
-          ></DataTable>
-        </div>
-      )}
+            {(lied.length === 0) ? (
+              <p>Loading...</p>
+            ) : (
+              <div>
+                <h2 className="text-center">Welches Lied gefällt Dir besser?</h2>
+                <CardGroup>
+                  <SongCard
+                    lied={lied[0].Liedanfang}
+                    quelle={lied[0].Quelle}
+                    seite={lied[0].Seite}
+                  />
+                  <SongCard
+                    lied={lied[1].Liedanfang}
+                    quelle={lied[1].Quelle}
+                    seite={lied[1].Seite}
+                  />
+                </CardGroup>
+              </div>
 
-    </div>
+            )}
+
+          </Row>
+        </Tab>
+        <Tab eventKey="competition-history" title="Rating history">
+          <Row id="competitionTable">
+
+            {(typeof competitionData === 'undefined') ? (
+              <p>Loading...</p>
+            ) : (
+              <div>
+                <div>
+                  <span>Suche Lied: </span><input type="text" onChange={handleCompetitionFilter}></input>
+                </div>
+                <DataTable
+                  columns={competitionColumns}
+                  data={competitionRecords}
+                  customStyles={customStyles}
+                  theme="light"
+                  fixedHeader
+                  pagination
+                ></DataTable>
+              </div>
+            )}
+
+          </Row>
+        </Tab>
+        <Tab eventKey="contact" title="Song rating">
+          <Row id="songData">
+
+            {(typeof songData === 'undefined') ? (
+              <p>Loading...</p>
+            ) : (
+              <div>
+                <div>
+                  <span>Suche Lied: </span><input type="text" onChange={handleSongFilter}></input>
+                </div>
+                <DataTable
+                  columns={songColumns}
+                  data={songRecords}
+                  customStyles={customStyles}
+                  theme="light"
+                  fixedHeader
+                  pagination
+                ></DataTable>
+              </div>
+            )}
+
+          </Row>
+        </Tab>
+      </Tabs>
+    </Container>
   )
 }
 
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,13 +1,62 @@
 from flask import Flask
+from flask import request
 import pandas as pd
+import trueskillthroughtime as ttt
+import datetime
+
+
+def prepare_song_rating():
+    songlist = pd.read_csv("../trueskill_tt/liederliste.csv")
+    songlist.insert(1, "Wertung", ttt.MU, True)
+    songlist.insert(2, "Unsicherheit", ttt.SIGMA, True)
+    
+    competition_history = pd.read_csv("../trueskill_tt/vergleiche.csv")
+    
+    return songlist
+
+
+def generate_competition_queue():
+    competition_list = songlist.sort_values(by=["Wertung"])
+    # print(competition_list.iloc[0:5, 0:3])
+    competition_list_1 = [title for idx, title in enumerate(competition_list["Liedanfang"].to_list()) if idx%2 == 0]
+    competition_list_2 = [title for idx, title in enumerate(competition_list["Liedanfang"].to_list()) if idx%2 == 1]
+    competition_queue = list(zip(competition_list_1, competition_list_2))  # drops entries if one list is longer
+    # print(competition_queue[-5:])
+    return competition_queue
+
+
+songlist = prepare_song_rating()
+competition_queue = generate_competition_queue()
+
 
 app = Flask(__name__)
 
-# Members API Route
+
 @app.route("/api/load_csv_as_dataframe/<string:filename>")
 def read_csv_as_dataframe(filename):
     return pd.read_csv("../trueskill_tt/"+filename).to_json(orient='records')
-    # return {"members": ["Member1", "Member2", "Member3"]}
+
+
+@app.route("/api/load_song_rating")
+def load_song_rating():
+    songlist = prepare_song_rating()
+    return songlist.to_json(orient='records')
+
+
+@app.route("/api/get_first_competition")
+def get_next_competition():
+    return songlist.loc[songlist['Liedanfang'].isin(competition_queue[0])].to_json(orient='records')
+
+
+@app.route('/api/declare_competition_winner', methods=['POST'])
+def declare_competition_winner():
+    winner = request.get_json()
+    last_competition = competition_queue.pop(0)
+    winner_index = 0 if last_competition[0] == winner else 1
+    competition_history = pd.read_csv("../trueskill_tt/vergleiche2.csv")
+    competition_history.loc[len(competition_history)] = [datetime.date.today().strftime("%Y-%m-%d"), last_competition[winner_index], last_competition[not winner_index]]
+    competition_history.to_csv("../trueskill_tt/vergleiche2.csv", index=False)
+    return get_next_competition(), 201
 
 
 if __name__ == "__main__":
